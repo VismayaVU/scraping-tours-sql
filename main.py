f2cfabe45721f8482e0feb094e62d7db28c67538
Vismaya <vadanavismaya@gmail.com>
2024-07-19 17:07:09 +0530

    Sending email when new event is shown on the website

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import requests
 import selectorlib
+import smtplib
+import ssl
+import os
+import time
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
@@ -19,7 +23,40 @@ def extract(source):
     return value
 
 
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "vadanavismaya@gmail.com"
+    password = str(os.getenv("password"))
+
+    receiver = "vadanavismaya@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+    print("Email was sent!")
+
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+        content = read(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store(extracted)
+                send_email(message="Hey, new event was found!")
+        time.sleep(2)
